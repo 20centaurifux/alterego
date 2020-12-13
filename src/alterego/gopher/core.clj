a8072d53ff24a1c41a520fb9238408805d13c3df
Sebastian Fedrau <sebastian.fedrau@gmail.com>
2020-12-13 17:34:37 +0100

    request logging (gopher)

--- src/alterego/gopher/core.clj
+++ src/alterego/gopher/core.clj
@@ -1,27 +1,37 @@
 (ns alterego.gopher.core
   (:gen-class)
   (:require [goophi.core :refer [info]]
-            [goophi.routing :refer [defroutes]]
+            [goophi.routing :refer [->routes]]
             [goophi.response :refer [menu-entity]]
             [goophi.fs :refer [get-contents]]
             [goophi.redirect :as html]
             [goophi.tcp :refer [->gopher-handler]]
+            [alterego.log :refer [log-request!]]
             [confick.core :as config]
             [aleph.tcp :as tcp]))
 
-(defroutes routes
-  ("URL\\:*"
-   [:as req]
-   (if-let [url (html/selector->url (:path req))]
-     (html/redirect url)
-     (menu-entity (info "Not found."))))
-  ("*"
-   [:as req]
-   (config/bind [^:required base-dir [:goophi :directory]]
-     (get-contents base-dir (:path req)))))
+(defn- log
+  [request]
+  (log-request! (:remote-addr request) :gopher (:path request))
+  request)
+
+(def ^:private routes
+  (->routes
+   ("URL\\:*"
+    [:as req]
+    (if-let [url (html/selector->url (:path req))]
+      (html/redirect url)
+      (menu-entity (info "Not found."))))
+   ("*"
+    [:as req]
+    (config/bind [^:required base-dir [:goophi :directory]]
+      (get-contents base-dir (:path req))))))
+
+(def ^:private app
+  (comp routes log))
 
 (defn start
   []
   (config/bind [^:required port [:goophi :binding :port]]
-    (tcp/start-server (->gopher-handler routes)
+    (tcp/start-server (->gopher-handler app)
                       {:port port})))
