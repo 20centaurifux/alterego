760b6d2c9301b6b0dac3e9b80fe933879a1671c8
Sebastian Fedrau <sebastian.fedrau@gmail.com>
2022-07-10 16:14:04 +0200

    updated dependencies

--- project.clj
+++ project.clj
@@ -4,20 +4,20 @@
   :license {:name "AGPLv3"
             :url "https://www.gnu.org/licenses/agpl-3.0"}
   :dependencies [[org.clojure/clojure "1.10.1"]
-                 [ring/ring-core "1.7.1"]
-                 [ring/ring-jetty-adapter "1.7.1"]
-                 [compojure "1.6.1"]
+                 [ring/ring-core "1.9.5"]
+                 [ring/ring-jetty-adapter "1.9.5"]
+                 [compojure "1.7.0"]
                  [hiccup "1.0.5"]
-                 [markdown-clj "1.0.7"]
-                 [org.clojure/core.cache "1.0.207"]
-                 [zcfux/confick "0.1.1"]
-                 [zcfux/goophi "0.1.0"]
-                 [org.clojure/data.json "1.0.0"]
-                 [aleph "0.4.6"]
-                 [toucan "1.15.1"]
-                 [org.xerial/sqlite-jdbc "3.32.3.2"]
-                 [org.clojure/core.async "1.3.610"]]
-  :plugins [[lein-asset-minifier "0.4.4"]]
+                 [markdown-clj "1.11.2"]
+                 [org.clojure/core.cache "1.0.225"]
+                 [zcfux/confick "0.1.2"]
+                 [zcfux/goophi "0.1.1"]
+                 [org.clojure/data.json "2.4.0"]
+                 [aleph "0.5.0-rc2"]
+                 [toucan "1.18.0"]
+                 [org.xerial/sqlite-jdbc "3.36.0.3"]
+                 [org.clojure/core.async "1.5.648"]]
+  :plugins [[lein-asset-minifier "0.4.6"]]
   :main ^:skip-aot alterego.core
   :profiles {:uberjar {:aot :all}}
   :minify-assets [[:js {:source ["node_modules/jquery/dist/jquery.js"
