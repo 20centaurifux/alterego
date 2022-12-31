027f72fc70c2fff4b25192b1d31df67068782fc2
Sebastian Fedrau <sebastian.fedrau@gmail.com>
2022-12-31 11:14:53 +0100

    updated dependencies

--- project.clj
+++ project.clj
@@ -3,20 +3,20 @@
   :url "https://github.com/20centaurifux"
   :license {:name "AGPLv3"
             :url "https://www.gnu.org/licenses/agpl-3.0"}
-  :dependencies [[org.clojure/clojure "1.10.1"]
-                 [ring/ring-core "1.9.5"]
-                 [ring/ring-jetty-adapter "1.9.5"]
+  :dependencies [[org.clojure/clojure "1.11.1"]
+                 [ring/ring-core "1.9.6"]
+                 [ring/ring-jetty-adapter "1.9.6"]
                  [compojure "1.7.0"]
                  [hiccup "1.0.5"]
-                 [markdown-clj "1.11.2"]
+                 [markdown-clj "1.11.4"]
                  [org.clojure/core.cache "1.0.225"]
-                 [zcfux/confick "0.1.2"]
-                 [zcfux/goophi "0.1.1"]
+                 [zcfux/confick "0.1.3"]
+                 [zcfux/goophi "0.1.2"]
                  [org.clojure/data.json "2.4.0"]
-                 [aleph "0.5.0-rc2"]
+                 [aleph "0.6.0"]
                  [toucan "1.18.0"]
-                 [org.xerial/sqlite-jdbc "3.36.0.3"]
-                 [org.clojure/core.async "1.5.648"]]
+                 [org.xerial/sqlite-jdbc "3.40.0.0"]
+                 [org.clojure/core.async "1.6.673"]]
   :plugins [[lein-asset-minifier "0.4.6"]]
   :main ^:skip-aot alterego.core
   :profiles {:uberjar {:aot :all}}
