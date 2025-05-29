b103d9fbfbb4550b0ca2d7d95cff1b48a4486d41
Sebastian Fedrau <sebastian.fedrau@gmail.com>
2025-05-29 12:56:32 +0200

    license + README

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject alterego "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
-            :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :description "My personal gopherspace and HTTP proxy."
+  :url "gopher://dixieflatline.de"
+  :license {:name "AGPLv3"
+            :url "https://www.gnu.org/licenses/agpl-3.0"}
   :dependencies [[org.clojure/clojure "1.11.1"]
                  [aleph "0.8.1"]
                  [hiccup "2.0.0-RC3"]
