eadafc4353c620d38481922ca1ef378d79e18315
Sebastian Fedrau <sebastian.fedrau@gmail.com>
2026-07-02 20:20:25 +0200

    chore: update dependencies and plugins in project.clj

--- project.clj
+++ project.clj
@@ -4,13 +4,13 @@
   :license {:name "AGPLv3"
             :url "https://www.gnu.org/licenses/agpl-3.0"}
   :dependencies [[org.clojure/clojure "1.11.1"]
-                 [aleph "0.8.1"]
-                 [hiccup "2.0.0-RC3"]
+                 [aleph "0.9.9"]
+                 [hiccup "2.0.0"]
                  [de.dixieflatline/confick "0.2.0-SNAPSHOT"]
-                 [de.dixieflatline/goophi "0.2.0-SNAPSHOT"]
-                 [integrant "0.13.1"]
-                 [ring/ring-core "1.13.0"]]
+                 [de.dixieflatline/goophi "0.2.1-SNAPSHOT"]
+                 [integrant "1.0.1"]
+                 [ring/ring-core "1.15.5"]]
   :main ^:skip-aot alterego.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}}
-  :plugins [[dev.weavejester/lein-cljfmt "0.13.0"]])
+  :plugins [[dev.weavejester/lein-cljfmt "0.16.4"]])
